9288ea0e7e3d484876584d0980f854cb251a480b
zykorwx <enrique.wx@gmail.com>
2013-08-07 12:35:00 -0500

    cambio de plugins

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , Facebook = require('facebook-node-sdk')
   , path = require('path');
+var graph = require('fbgraph');
+var everyauth = require('everyauth');
+var mongo = require('mongoose');
 
 var app = express();
 
@@ -18,10 +20,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
+app.use(express.bodyParser('viewor'));
 app.use(express.cookieParser());
 app.use(express.session({ secret: 'viewor' }));
-app.use(Facebook.middleware({ appId: '490289871056517', secret: 'fdab8b77b54df41dd4f955b24e101c93' }));
+app.use(everyauth.middleware(app));
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -33,12 +35,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-app.get('/facebook', Facebook.loginRequired(), function (req, res) {
-  req.facebook.api('/me', function(err, user) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, ' + user.name + '! ' + user.id + ' - ' + user.email );
-  });
-});
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
